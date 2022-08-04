ebc920c3ba8a994c7567393b36246ec2ab2d59e9
baaami <lgh121546@gmail.com>
2022-08-04 22:35:29 +0900

    ReFactoring, Using singleton pattern

---
+++ blockchain/blockchain.go
@@ -0,0 +1,60 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
+type block struct {
+	Data     string
+	Hash     string
+	PrevHash string
+}
+
+type blockchain struct {
+	blocks []*block
+}
+
+// sigleton
+var b *blockchain
+var once sync.Once
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func (b *blockchain) getLastHash() string {
+	totalBlocks := len(GetBlockchain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockchain().blocks[totalBlocks-1].Hash
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", b.getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
+
+// singleton 인스턴스 획득
+func GetBlockchain() *blockchain {
+	if b == nil {
+		// 처음 초기화하는 부분은 반드시 1번만 수행되도록
+		once.Do(func() {
+			b = &blockchain{}
+			b.AddBlock("Genesis")
+		})
+	}
+	return b
+}
+
+func (b *blockchain) AllBlocks() []*block {
+	return b.blocks
+}
