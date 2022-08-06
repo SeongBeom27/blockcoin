af0ebb4c1d2e9881879422a7d59a98e5ebe98854
baaami <52270698+SeongBeom27@users.noreply.github.com>
2022-08-06 22:16:48 +0900

    1. json Marshaling 방법 2. struct field tags 사용법 ex) `json:"payload,omitempty"`

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "github.com/baaami/blockcoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	// `json:"url"` : struct field tags 사용
+	URL			string 	`json:"url"`
+	Method 		string 	`json:"method"`
+	Description string 	`json:"description"`
+	// omitempty : data가 비어있지 않은 경우에만 출력
+	Payload		string	`json:"payload,omitempty"`
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription {
+		{			
+			URL: "/",
+			Method: "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL: "/blokcs",
+			Method: "POST",
+			Description: "See Documentation",
+			Payload: "data:string",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
+
+	// b, err := json.Marshal(data)
+	// utils.HandleErr(err)
+	// fmt.Fprintf(rw, "%s", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
