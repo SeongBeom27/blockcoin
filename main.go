94f8c1b19224a1cd81831f4923efca4bb4b6e35e
baaami <52270698+SeongBeom27@users.noreply.github.com>
2022-08-06 23:04:31 +0900

    1. POST API 추가 (블록 추가)

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/baaami/blockcoin/blockchain"
+	"github.com/baaami/blockcoin/utils"
 )
 
 const port string = ":4000"
@@ -18,6 +21,10 @@ type URLDescription struct {
 	Payload		string	`json:"payload,omitempty"`
 }
 
+type AddBlockBody struct {
+	Message string
+}
+
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription {
 		{			
@@ -26,22 +33,44 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Description: "See Documentation",
 		},
 		{
-			URL: "/blokcs",
+			URL: "/blocks",
 			Method: "POST",
 			Description: "See Documentation",
 			Payload: "data:string",
 		},
 	}
 	rw.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(rw).Encode(data)
-
+	
 	// b, err := json.Marshal(data)
 	// utils.HandleErr(err)
 	// fmt.Fprintf(rw, "%s", b)
+
+	// input writer -> data Marshaling -> transfer json data to writer
+	json.NewEncoder(rw).Encode(data)
+
+}
+
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		// {"data": "my block data"}
+
+		// json into struct
+
+		// 1. data 구조 확인
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
 }
 
 func main() {
 	http.HandleFunc("/", documentation)
+	http.HandleFunc("/blocks", blocks)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
