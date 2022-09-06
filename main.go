0bd8e659a405cdd83b689b0d57f1dbc4ea72626d
baaami <lgh121546@naver.com>
2022-09-06 18:17:39 +0900

    GoRoutine 복습

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"github.com/baaami/blockcoin/cli"
+	"fmt"
+	"time"
 )
 
+func countTen(c chan<- int) {
+	for i := range [10]int{} {
+		time.Sleep(1 * time.Second)
+		c <- i
+	}
+	close(c)
+}
+
+func receive(c <-chan int) {
+	// blocking operation
+	for {
+		a, ok := <-c
+		if !ok {
+			fmt.Println("we are done")
+			break
+		}
+
+		fmt.Printf("received: %d\n", a)
+	}
+}
+
 func main() {
-	cli.Start()
+	c := make(chan int)
+	go countTen(c)
+	receive(c)
 }
